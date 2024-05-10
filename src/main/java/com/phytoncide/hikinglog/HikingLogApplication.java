5057796116a9d1c2ada9620afb5478b1f8b50534
yuchaemin2 <kusuri94@gmail.com>
2024-05-10 21:31:48 +0900

    first commit

---
+++ src/main/java/com/phytoncide/hikinglog/HikingLogApplication.java
@@ -0,0 +1,13 @@
+package com.phytoncide.hikinglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikingLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HikingLogApplication.class, args);
+    }
+
+}
