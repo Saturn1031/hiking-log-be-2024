266610e3277726e45771e8b136d93626ea5f3709
Narae Gwon <rnjs021031@gmail.com>
2024-05-19 18:25:05 +0900

    [Feat] 게시판 삭제

--- src/main/java/com/phytoncide/hikinglog/domain/boards/repository/ImageRepository.java
+++ src/main/java/com/phytoncide/hikinglog/domain/boards/repository/ImageRepository.java
@@ -3,6 +3,8 @@ package com.phytoncide.hikinglog.domain.boards.repository;
 import com.phytoncide.hikinglog.domain.boards.entity.ImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ImageRepository extends JpaRepository<ImageEntity, Integer> {
+import java.util.List;
 
+public interface ImageRepository extends JpaRepository<ImageEntity, Integer> {
+    List<ImageEntity> findAllByBoardEntity_Bid(Integer bid);
 }
