a9ee618635e48fef8b78c9efea4bea0ae7e7fe59
Narae Gwon <rnjs021031@gmail.com>
2024-05-19 20:50:24 +0900

    [Feat] 게시판 수정

--- src/main/java/com/phytoncide/hikinglog/domain/boards/controller/BoardsController.java
+++ src/main/java/com/phytoncide/hikinglog/domain/boards/controller/BoardsController.java
@@ -1,19 +1,14 @@
 package com.phytoncide.hikinglog.domain.boards.controller;
 
-import com.phytoncide.hikinglog.domain.boards.dto.BoardImageUploadDTO;
 import com.phytoncide.hikinglog.domain.boards.dto.BoardWriteDTO;
 import com.phytoncide.hikinglog.domain.boards.service.BoardService;
 import com.phytoncide.hikinglog.domain.member.config.AuthDetails;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -33,5 +28,13 @@ public class BoardsController {
     public String boardDelete(@PathVariable("boardId") Integer boardId,
                               @AuthenticationPrincipal AuthDetails authDetails) {
         return boardService.deleteBoard(boardId, authDetails);
+    }
+
+    @PatchMapping("/{boardId}")
+    public String boardUpdate(@PathVariable("boardId") Integer boardId,
+                              @RequestPart("data") BoardWriteDTO boardWriteDTO,
+                              @RequestPart(value = "images", required = false) List<MultipartFile> images,
+                              @AuthenticationPrincipal AuthDetails authDetails) throws IOException {
+        return boardService.updateBoard(boardId, boardWriteDTO, images, authDetails);
     }
 }
--- src/main/java/com/phytoncide/hikinglog/domain/boards/service/BoardService.java
+++ src/main/java/com/phytoncide/hikinglog/domain/boards/service/BoardService.java
@@ -1,6 +1,5 @@
 package com.phytoncide.hikinglog.domain.boards.service;
 
-import com.phytoncide.hikinglog.domain.boards.dto.BoardImageUploadDTO;
 import com.phytoncide.hikinglog.domain.boards.dto.BoardWriteDTO;
 import com.phytoncide.hikinglog.domain.boards.entity.BoardEntity;
 import com.phytoncide.hikinglog.domain.boards.entity.ImageEntity;
@@ -13,11 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -72,7 +68,7 @@ public class BoardService {
 //                    is.close();
 //                }
 
-                    ImageEntity imageEntity = boardWriteDTO.toImageEntity(originalFileName, storedFileName);
+                    ImageEntity imageEntity = boardWriteDTO.toImageEntity(boardEntity, originalFileName, storedFileName);
                     imageRepository.save(imageEntity);
                 }
             }
@@ -83,6 +79,9 @@ public class BoardService {
 
     public String deleteBoard(Integer boardId, AuthDetails authDetails) {
         Integer userId = authDetails.getMemberEntity().getUid();
+        if (boardRepository.findById(boardId).isEmpty()) {
+            return "유효하지 않은 게시글 아이디입니다.";
+        }
         Integer boardUserId = boardRepository.findById(boardId).get().getMemberEntity().getUid();
 
         if (!boardUserId.equals(userId)) {
@@ -103,5 +102,83 @@ public class BoardService {
         boardRepository.deleteById(boardId);
 
         return "게시글 삭제에 성공했습니다.";
+    }
+
+    public String updateBoard(Integer boardId, BoardWriteDTO boardWriteDTO, List<MultipartFile> files, AuthDetails authDetails) throws IOException {
+        Integer userId = authDetails.getMemberEntity().getUid();
+        if (boardRepository.findById(boardId).isEmpty()) {
+            return "유효하지 않은 게시글 아이디입니다.";
+        }
+        Integer boardUserId = boardRepository.findById(boardId).get().getMemberEntity().getUid();
+
+        if (!boardUserId.equals(userId)) {
+            return "게시글 수정 권한이 없습니다.";
+        }
+
+        if (boardWriteDTO.getTitle().isEmpty()) {
+            return "제목을 작성해주세요.";
+        }
+        if (boardWriteDTO.getContent().isEmpty()) {
+            return "내용을 작성해주세요.";
+        }
+
+        String email = authDetails.getUsername();
+        MemberEntity memberEntity = memberRepository.findByEmail(email);
+
+        BoardEntity boardEntity = boardRepository.findById(boardId).get();
+
+        boardEntity.setTitle(boardWriteDTO.getTitle());
+        boardEntity.setContent(boardWriteDTO.getContent());
+        boardEntity.setTag(boardWriteDTO.getTag());
+
+        List<ImageEntity> images = imageRepository.findAllByBoardEntity_Bid(boardId);
+        if (!images.isEmpty()) {
+            for (ImageEntity image: images) {
+                String filename = image.getStoredFileName();
+                Integer fileId = image.getIid();
+                // S3에서 이미지 삭제하는 로직 추가
+
+                imageRepository.deleteById(fileId);
+            }
+        }
+
+        if (files != null && !files.isEmpty()) {
+            for (MultipartFile file : files) {
+                if (file != null && !file.isEmpty()) {
+                    String originalFileName = file.getOriginalFilename();
+
+                    String extention = originalFileName.substring(originalFileName.lastIndexOf("."));
+
+                    UUID uuid = UUID.randomUUID();
+                    String storedFileName = uuid + "_" + originalFileName;
+
+                    // S3에 파일 저장하는 로직 추가
+//                InputStream is = file.getInputStream();
+//                byte[] bytes = IOUtils.toByteArray(is);
+//
+//                ObjectMetadata metadata = new ObjectMetadata();
+//                metadata.setContentType("image/" + extention);
+//                metadata.setContentLength(bytes.length);
+//                ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+//
+//                try{
+//                    PutObjectRequest putObjectRequest =
+//                            new PutObjectRequest(bucketName, storedFileName, byteArrayInputStream, metadata)
+//                                    .withCannedAcl(CannedAccessControlList.PublicRead);
+//                    amazonS3.putObject(putObjectRequest); // put image to S3
+//                }catch (Exception e){
+//                    throw new S3Exception(ErrorCode.PUT_OBJECT_EXCEPTION);
+//                }finally {
+//                    byteArrayInputStream.close();
+//                    is.close();
+//                }
+
+                    ImageEntity imageEntity = boardWriteDTO.toImageEntity(boardEntity, originalFileName, storedFileName);
+                    imageRepository.save(imageEntity);
+                }
+            }
+        }
+
+        return "게시글 수정에 성공했습니다.";
     }
 }
