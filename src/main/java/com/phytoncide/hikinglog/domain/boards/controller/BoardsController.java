fffd381ef957b8470030c47ea928bf4064285df9
Narae Gwon <rnjs021031@gmail.com>
2024-05-24 20:45:11 +0900

    [Feat] 게시판 목록 조회

--- src/main/java/com/phytoncide/hikinglog/domain/boards/controller/BoardsController.java
+++ src/main/java/com/phytoncide/hikinglog/domain/boards/controller/BoardsController.java
@@ -1,9 +1,13 @@
 package com.phytoncide.hikinglog.domain.boards.controller;
 
+import com.phytoncide.hikinglog.domain.boards.dto.BoardListResponseDTO;
 import com.phytoncide.hikinglog.domain.boards.dto.BoardWriteDTO;
+import com.phytoncide.hikinglog.domain.boards.dto.CursorPageRequestDto;
+import com.phytoncide.hikinglog.domain.boards.entity.BoardEntity;
 import com.phytoncide.hikinglog.domain.boards.service.BoardService;
 import com.phytoncide.hikinglog.domain.member.config.AuthDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -36,5 +40,24 @@ public class BoardsController {
                               @RequestPart(value = "images", required = false) List<MultipartFile> images,
                               @AuthenticationPrincipal AuthDetails authDetails) throws IOException {
         return boardService.updateBoard(boardId, boardWriteDTO, images, authDetails);
+    }
+
+    @GetMapping("")
+    public BoardListResponseDTO boardRead(@RequestBody CursorPageRequestDto cursorPageRequestDto) {
+        Integer size = cursorPageRequestDto.getSize();
+        Integer page = cursorPageRequestDto.getPage();
+        List<BoardListResponseDTO.BoardResponseDTO> boardList = boardService.readeBoards(size, page);
+
+        if (!boardService.hasNextBoards(size, page)) {
+            return BoardListResponseDTO.builder()
+                    .boardList(boardList)
+                    .hasNext(false)
+                    .build();
+        }
+
+        return BoardListResponseDTO.builder()
+                .boardList(boardList)
+                .hasNext(true)
+                .build();
     }
 }
