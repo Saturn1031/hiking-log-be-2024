d6ea8ec79485bc26de6b0a3c2ced9b9fefc25255
ninano05 <ohjy0530@gmail.com>
2024-05-31 18:43:38 +0900

    feat: aws S3 연결

--- src/main/java/com/phytoncide/hikinglog/domain/member/controller/MemberController.java
+++ src/main/java/com/phytoncide/hikinglog/domain/member/controller/MemberController.java
@@ -1,7 +1,9 @@
 package com.phytoncide.hikinglog.domain.member.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.phytoncide.hikinglog.base.code.ResponseCode;
 import com.phytoncide.hikinglog.base.dto.ResponseDTO;
+import com.phytoncide.hikinglog.domain.awsS3.AmazonS3Service;
 import com.phytoncide.hikinglog.domain.member.Service.MemberService;
 import com.phytoncide.hikinglog.domain.member.config.SecurityConfig;
 import com.phytoncide.hikinglog.domain.member.dto.JoinDTO;
@@ -11,6 +13,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/member")
@@ -18,11 +23,19 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
     private final MemberRepository memberRepository;
     private final SecurityConfig securityConfig;
+    private final AmazonS3Service amazonS3Service;
     private final MemberService memberService;
 
     @PostMapping("/join")
-    public ResponseEntity<ResponseDTO> join(@RequestBody JoinDTO joinDTO) {
-        String res = memberService.join(joinDTO);
+    public ResponseEntity<ResponseDTO> join(@RequestParam("user") String joinDTO, @RequestParam("image") MultipartFile multipartFile) throws IOException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        JoinDTO mapperJoinDTO = mapper.readValue(joinDTO, JoinDTO.class);
+
+        String imageFile = amazonS3Service.saveFile(multipartFile);
+        mapperJoinDTO.setImage(imageFile);
+
+        String res = memberService.join(mapperJoinDTO);
 
         return ResponseEntity
                 .status(ResponseCode.SUCCESS_JOIN.getStatus().value())
