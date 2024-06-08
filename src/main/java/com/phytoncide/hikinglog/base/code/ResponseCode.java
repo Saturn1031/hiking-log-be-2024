55d12993461c026bb95c051d84c8f86dbd1a6404
ninano05 <ohjy0530@gmail.com>
2024-06-08 18:16:38 +0900

    [feat] 프로필 수정, 조회

--- src/main/java/com/phytoncide/hikinglog/base/code/ResponseCode.java
+++ src/main/java/com/phytoncide/hikinglog/base/code/ResponseCode.java
@@ -13,6 +13,8 @@ public enum ResponseCode {
     SUCCESS_FIND_EMAIL(HttpStatus.OK, "이메일을 찾았습니다."),
     SUCCESS_CHANGE_PASSWORD(HttpStatus.OK, "비밀번호 변경을 성공했습니다."),
     SUCCESS_FIND_PASSWORD(HttpStatus.OK, "비밀번호를 찾았습니다."),
+    SUCCESS_UPDATE_PROFILE(HttpStatus.OK, "프로필 정보를 업데이트 했습니다."),
+    SUCCESS_GET_PROFILE(HttpStatus.OK, "프로필을 조회했습니다.")
     ;
 
     private final HttpStatus status;
