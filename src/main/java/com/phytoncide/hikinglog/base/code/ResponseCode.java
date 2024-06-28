60391e028fe3d1fcdf7cc17eeddec47815e83ea4
ninano05 <ohjy0530@gmail.com>
2024-06-28 15:45:30 +0900

    위치기반 숙박시설 목록 조회

--- src/main/java/com/phytoncide/hikinglog/base/code/ResponseCode.java
+++ src/main/java/com/phytoncide/hikinglog/base/code/ResponseCode.java
@@ -14,7 +14,8 @@ public enum ResponseCode {
     SUCCESS_CHANGE_PASSWORD(HttpStatus.OK, "비밀번호 변경을 성공했습니다."),
     SUCCESS_FIND_PASSWORD(HttpStatus.OK, "비밀번호를 찾았습니다."),
     SUCCESS_UPDATE_PROFILE(HttpStatus.OK, "프로필 정보를 업데이트 했습니다."),
-    SUCCESS_GET_PROFILE(HttpStatus.OK, "프로필을 조회했습니다.")
+    SUCCESS_GET_PROFILE(HttpStatus.OK, "프로필을 조회했습니다."),
+    SUCCESS_GET_ACCOMMODATION_LIST(HttpStatus.OK, "숙소 목록을 조회했습니다."),
     ;
 
     private final HttpStatus status;
