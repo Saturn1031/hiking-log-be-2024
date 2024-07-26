4572a7dc12175e548d86575f30b35ab4250c316c
ninano05 <ohjy0530@gmail.com>
2024-07-26 20:48:05 +0900

    [fix] 오류 코드 주석 처리

--- src/main/java/com/phytoncide/hikinglog/domain/mountain/repository/MountainRepository.java
+++ src/main/java/com/phytoncide/hikinglog/domain/mountain/repository/MountainRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MountainRepository extends JpaRepository<MountainEntity, Integer> {
     MountainEntity findByMntilistno(Integer mntilistno);
-    boolean existsByMName(String mName);
-    MountainEntity findByMName(String mName);
+//    boolean existsByMName(String mName);
+//    MountainEntity findByMName(String mName);
 }
